13a012690eb5eda96ad8520c0856e5dc79cd39b4
osvaldo <osvaldo.baeza2601@alumnos.ubiobio.cl>
2026-09-21 10:18:12 -0300

    Avance Region.java

---
+++ src/Region.java
@@ -0,0 +1,40 @@
+package src;
+
+public class Region {
+    private  int codigo;
+    private  String nombre;
+
+    public Region(int cod, String nom) {
+        this.codigo = cod;
+        this.nombre = nom;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public boolean addComuna(int codigo, String nombre) {
+
+    }
+
+    public Comuna findComunaById(int codigo) {
+
+    }
+    public Comuna[] getComunas() {
+
+    }
+    public int getCantidadEstaciones() {
+
+    }
+
+
+
+
+
+
+
+
+
+    }
